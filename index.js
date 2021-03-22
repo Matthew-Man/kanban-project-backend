7bf6d1403e98fba1f90e92d3f611ef3b330d0da2
Matthew Man <matthew08m@gmail.com>
2021-03-22 17:54:17 +0000

    testing db uri failure

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config();
 app.use(cors());
 app.use(express.json()) //Parse JSON body in requests
 
-
+console.log(process.env.DATABASE_URI ? process.env.DATABASE_URI.length : "No DB URI")
 const client = new Client({
     // database: "kanbanproject"
     connectionString: process.env.DATABASE_URI,
